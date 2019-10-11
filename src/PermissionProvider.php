5723540700afd93377f75f627d2bdb03a9bf2299
Michael Aben <m.aben@live.nl>
2019-10-11 10:47:51 +0200

    init

---
+++ src/PermissionProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace MabenDev\Permissions;
+
+use Illuminate\Support\ServiceProvider;
+
+class PermissionProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
