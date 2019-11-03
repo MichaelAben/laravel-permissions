517719446a5f1c30a853e244e20a745e0e40885d
Michael Aben <m.aben@live.nl>
2019-11-03 16:27:00 +0100

    Update BladeExtentions.php

--- src/BladeExtentions.php
+++ src/BladeExtentions.php
@@ -4,6 +4,7 @@
 namespace MabenDev\Permissions;
 
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Blade;
 
 /**
@@ -20,36 +21,43 @@ class BladeExtentions
     public static function register()
     {
         Blade::if('hasPermission', function ($permission) {
+            if(!Auth::check()) return false;
             $user = config('MabenDevPermissions.user')();
             return $user->hasPermission($permission);
         });
 
         Blade::if('hasAnyPermission', function ($permissions) {
+            if(!Auth::check()) return false;
             $user = config('MabenDevPermissions.user')();
             return $user->hasAnyPermission($permissions);
         });
 
         Blade::if('hasAllPermissions', function ($permissions) {
+            if(!Auth::check()) return false;
             $user = config('MabenDevPermissions.user')();
             return $user->hasAllPermissions($permissions);
         });
 
         Blade::if('hasPermissionIn', function ($permission) {
+            if(!Auth::check()) return false;
             $user = config('MabenDevPermissions.user')();
             return $user->hasPermissionIn($permission);
         });
 
         Blade::if('hasRole', function ($role) {
+            if(!Auth::check()) return false;
             $user = config('MabenDevPermissions.user')();
             return $user->hasRole($role);
         });
 
         Blade::if('hasAnyRole', function ($roles) {
+            if(!Auth::check()) return false;
             $user = config('MabenDevPermissions.user')();
             return $user->hasAnyRole($roles);
         });
 
         Blade::if('hasAllRoles', function ($roles) {
+            if(!Auth::check()) return false;
             $user = config('MabenDevPermissions.user')();
             return $user->hasAllRoles($roles);
         });
