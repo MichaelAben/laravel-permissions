6a5ad668260d03ce9446794390a49c8489b8f9af
Michael Aben <m.aben@live.nl>
2020-01-27 09:24:25 +0100

    Update PermissionProvider.php

--- src/PermissionProvider.php
+++ src/PermissionProvider.php
@@ -2,6 +2,7 @@
 
 namespace MabenDev\Permissions;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
